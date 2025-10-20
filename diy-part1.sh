01eaf3ee54866550110bb5ec912deebb79e5e3ee
lim128 <32699966+lim128@users.noreply.github.com>
2025-10-20 22:16:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,4 @@
 git clone -b beta --single-branch https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone -b v5 --single-branch https://github.com/sbwml/openwrt_helloworld.git package/helloworld
 git clone -b v5 --single-branch https://github.com/sbwml/luci-app-mosdns.git package/luci-app-mosdns
-git clone -b main --single-branch https://github.com/sbwml/luci-app-openlist2.git package/openlist
+git clone -b main --single-branch https://github.com/sbwml/luci-app-openlist2.git package/luci-app-openlist2
