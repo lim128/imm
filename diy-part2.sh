ee8d5c05302239af29300d2071c18b4a8b6709ac
lim128 <32699966+lim128@users.noreply.github.com>
2025-10-20 21:39:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,7 +21,7 @@
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 PKGS=(
   luci-app-openlist luci-app-openclash luci-app-passwall luci-app-daed golang chinadns-ng daed dns2socks-rust dns2socks dns2tcp geoview
-  hysteria ipt2socks lua-neturl microsocks mosdns naiveproxy nikki pdnsd redsocks2 shadow-tls shadowsocks-libev shadowsocks-rust shadowsocksr-libev simple-obfs sing-box
+  hysteria ipt2socks lua-neturl microsocks mosdns naiveproxy nikki openlist pdnsd redsocks2 shadow-tls shadowsocks-libev shadowsocks-rust shadowsocksr-libev simple-obfs sing-box
   tcping trojan-plus trojan tuic-client v2ray-core v2ray-geodata v2ray-plugin xray-core xray-plugin
 )
 for pkg in "${PKGS[@]}"; do
