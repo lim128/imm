bdef2724c86723329fc005297cdd6d383d1a16bf
lim128 <32699966+lim128@users.noreply.github.com>
2025-10-20 22:01:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,11 +20,12 @@
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 PKGS=(
-  luci-app-openlist luci-app-openclash luci-app-passwall luci-app-daed golang chinadns-ng daed dns2socks-rust dns2socks dns2tcp geoview
+  luci-app-homeproxy luci-app-openlist luci-app-openclash luci-app-passwall luci-app-daed golang chinadns-ng daed dns2socks-rust dns2socks dns2tcp geoview
   hysteria ipt2socks lua-neturl microsocks mosdns naiveproxy nikki openlist pdnsd redsocks2 shadow-tls shadowsocks-libev shadowsocks-rust shadowsocksr-libev simple-obfs sing-box
   tcping trojan-plus trojan tuic-client v2ray-core v2ray-geodata v2ray-plugin xray-core xray-plugin
 )
 for pkg in "${PKGS[@]}"; do
+  find feeds/helloworld -type d -name "$pkg" -exec rm -rf {} +
   find feeds/luci -type d -name "$pkg" -exec rm -rf {} +
   find feeds/packages -type d -name "$pkg" -exec rm -rf {} +
 done
