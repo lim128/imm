a679ec9b7aca6aee4775616d3f96f317326e4bce
lim128 <32699966+lim128@users.noreply.github.com>
2025-10-21 01:20:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,7 +18,7 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 PKGS=(
   luci-app-openlist luci-app-openclash luci-app-passwall luci-app-daed golang chinadns-ng daed dns2socks-rust dns2socks dns2tcp geoview
   hysteria ipt2socks lua-neturl microsocks mosdns naiveproxy nikki openlist pdnsd redsocks2 shadow-tls shadowsocks-libev shadowsocks-rust shadowsocksr-libev simple-obfs sing-box
